133f287c57905326e1f71f76ef536c9e7fcc3e96
Andras Schifferer <schifferer.andras@gmail.com>
2023-12-08 18:54:55 +0100

    Matlab plotting for class (HW2 )

--- HW2/codes/Problem.m
+++ HW2/codes/Problem.m
@@ -4,23 +4,29 @@ classdef Problem
         method
         name
         func
+        parname="\lambda"
 
         A
         b
         n %size of x
         m %size of b
+        p %size(s,1)
 
         U
         s
         sm
         V
         L = []
-
         X = []
+
+        Xs
         pars %lambda or k
         rho = [] %residual norm
         eta = [] %solution norm
         F = [] %each column is one set of filter factors corresponding to lambda_i
+        gcv_vals = []
+        ind_best_L = []
+        ind_best_gcv = []
     end
     
     methods
@@ -32,20 +38,23 @@ classdef Problem
             obj.n = size(A, 2);
             obj.m = size(A, 1);
       
-            if nargin<6
+            if nargin<7
                 if isempty(L)
                     [U, s, V] = csvd(A);
                 else
-                    [U, sm, X, V] = csvd(A);
+                    [U, s, X, V] = cgsvd(A, L);
                 end
                 
             end
             if isempty(L)
                obj.U = U; obj.s = s; obj.V = V;
             else
-               obj.U = U; obj.sm = sm; obj.V = V; obj.X = X;
+               obj.U = U; obj.sm = s; obj.V = V; obj.X = X;
             end
             
+            obj.p = size(s,1);
+
+            obj.L = L;
             obj.pars = pars;
            
             if strcmp(obj.method, 'Tikh')
@@ -55,51 +64,141 @@ classdef Problem
 
         function obj=gen_data(obj)
             npars = length(obj.pars);
-            obj.eta = zeros(npars);
-            obj.rho = zeros(npars);
-            obj.X = zeros(obj.n,npars);
-
+            obj.eta = zeros(npars,1);
+            obj.rho = zeros(npars,1);
+            obj.Xs = zeros(obj.n,npars);
+            %% Solutions
             if strcmp(obj.method, 'Tikh')
-                x0 = zeros(obj.n,1);
                 if isempty(obj.L)
-                    [x_lambda, rho, eta] = tikhonov(obj.U, obj.s, obj.V, obj.b, obj.pars, x0); %#ok<PROP> 
+                    [x_lambda, rho, eta] = obj.func(obj.U, obj.s, obj.V, obj.b, obj.pars); %#ok<PROP> 
                 else
-                    [x_lambda, rho, eta] = tikhonov(obj.U, obj.sm, obj.X, obj.b, obj.pars, x0); %#ok<PROP> 
+                    [x_lambda, rho, eta] = obj.func(obj.U, obj.sm, obj.X, obj.b, obj.pars); %#ok<PROP> 
                 end
             end
 
-            obj.X = x_lambda;
+            %% Filter factors
+            if strcmp(obj.method, 'Tikh')
+                if isempty(obj.L)
+                    F = fil_fac(obj.s,obj.pars, obj.method); %#ok<PROP> 
+                else
+                    F = fil_fac(obj.sm, obj.pars, obj.method); %#ok<PROP> 
+                end
+            end
+
+            obj.Xs = x_lambda;
             obj.rho = rho; %#ok<PROP> 
             obj.eta = eta; %#ok<PROP> 
+            obj.F = F; %#ok<PROP> 
+            % L curve
+            [obj.ind_best_L,~] = find_largest_curvature(obj.rho,obj.eta, obj.pars);
+            % GCV
+            obj.gcv_vals = obj.rho.^2./(obj.m-obj.n+obj.p-sum(obj.F)').^2;
+            [~, obj.ind_best_gcv] = min(obj.gcv_vals);
+
         end
        
         function plot_l_curve(obj, fig, ax)
-            put_legend = false;
             if nargin<3
               fig = figure;
               ax = gca;
-              put_legend = true;
             end
-            [best_ind, best_lambda] = find_largest_curvature(obj.eta, obj.rho, obj.pars);
-            fig;
-            hold on 
             
+            fig;
+
             grid on 
             set(ax, "XScale", "log")
             set(ax, "YScale", "log")
-            curve=plot(ax, obj.eta, obj.rho);
+            curve=plot(ax, obj.rho, obj.eta, "--");
+            best_ind = obj.ind_best_L; best_lambda = obj.pars(best_ind);
+            best_text = sprintf("$%s = %g$",obj.parname, best_lambda);
+            best_point = scatter(ax, obj.rho(best_ind), obj.eta(best_ind),100, "X");
+
             xlabel("$\left\| A \textbf{x} - \textbf{b}\right\|$","Interpreter","latex")
             ylabel("$\left\| \textbf{x} \right\|$","Interpreter","latex")
-            scatter(ax, obj.eta(best_ind), obj.rho(best_ind))
-            if put_legend
-                set(curve, "DisplayName", obj.name)
-                legend("Interpreter","latex")
+            
+            
+            if nargin<3
+                title(sprintf("%s (best %s)", ...
+                    obj.name, best_text), ...
+                    "Interpreter","latex");
             else
-                title(obj.name,"Interpreter","latex")
+                title("L curves");
+                set(best_point, "DisplayName", best_text);
+                set(curve, "DisplayName", obj.name);
+                legend("Interpreter","latex")
             end
             
 
-            hold off
+
+
+        end
+
+
+        function plot_gcv_curve(obj, fig, ax)
+            if nargin<3
+              fig = figure;
+              ax = gca;
+            end
+            
+            fig;
+
+            grid on 
+            set(ax, "XScale", "log")
+            set(ax, "YScale", "log")
+            curve=plot(ax, obj.pars, obj.gcv_vals, "--");
+            best_ind = obj.ind_best_gcv; best_lambda = obj.pars(best_ind);
+            best_text = sprintf("$%s = %g$",obj.parname, best_lambda);
+            best_point = scatter(ax, obj.pars(best_ind), obj.gcv_vals(best_ind),100, "X");
+
+            xlabel(sprintf("$%s$", obj.parname),"Interpreter","latex")
+            ylabel("$G$","Interpreter","latex")
+            
+            
+            if nargin<3
+                title(sprintf("%s (best %s)", ...
+                    obj.name, best_text), ...
+                    "Interpreter","latex");
+            else
+                title("GCV");
+                set(best_point, "DisplayName", best_text);
+                set(curve, "DisplayName", obj.name);
+                legend("Interpreter","latex")
+            end
+            
+
+
+
+        end
+
+        function plot_best_sol(obj,ts,type_of_sol, fig, ax)
+            
+            fig;
+
+            grid on 
+%             set(ax, "XScale", "log")
+%             set(ax, "YScale", "log")
+            
+            if strcmp(type_of_sol, 'GCV') || type_of_sol == 2
+                best_ind = obj.ind_best_gcv;
+                best_type = "GCV";
+            elseif strcmp(type_of_sol, 'L') || type_of_sol == 1
+                best_ind = obj.ind_best_L;
+                best_type="L curve";
+            end
+            curve=plot(ax, ts, obj.Xs(:, best_ind));
+
+            best_lambda = obj.pars(best_ind);
+            best_text = sprintf("%s (%s)",obj.name, best_type);
+            
+            xlabel("$t$","Interpreter","latex")
+            ylabel("$x(t)$","Interpreter","latex")
+            
+            
+            title("Solution");
+            set(curve, "DisplayName", best_text);
+            legend("Interpreter","latex")
+            
+
 
 
         end
--- HW2/codes/watchmaker_plots.m
+++ HW2/codes/watchmaker_plots.m
@@ -8,10 +8,26 @@ cond(K)
 n = size(K, 2);
 m = size(K, 1);
 
-[U, s, V] = csvd(K);
+%[U, s, V] = csvd(K);
 p1 = Problem(K, g, 'Tikh', "Tikhonov simple", [], ...
-    logspace(-13, -1, 300), U, s, V);
+    logspace(-12, 2, 300));%, U, s, V);
 p1 = gen_data(p1);
+
+%L = eye(m, n);
+L = get_l(n, 2);
+%[Ug, sm, Xg, Vg] = cgsvd(K, L);
+p2 = Problem(K, g, 'Tikh', "Tikhonov advanced", L, ...
+    logspace(-12, 2, 300));%, Ug, sm, Vg, Xg);
+p2 = gen_data(p2);
+
 %loglog(p1.eta, p1.rho)
-plot_l_curve(p1)
+fig = figure;
+ax = gca;
+hold on
+ts = linspace(0,10,n)';
+% plot_gcv_curve(p1, fig, ax)
+% plot_gcv_curve(p2, fig, ax)
+plot_best_sol(p1,ts,1,  fig, ax)
+plot_best_sol(p2,ts,1,  fig, ax)
+hold off
 
